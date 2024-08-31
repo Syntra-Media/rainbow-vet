2c07db54e1b6ef3cb1ed2454c9f2123a49eccd28
emir <reostraemir@gmail.com>
2024-08-31 10:17:30 +0300

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} bg-section`}>{children}</body>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XE1TL8HD3C"></Script>
+            <Script id="google-analytics" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+    
+                    gtag('config', 'G-XE1TL8HD3C');
+                    `}
+            </Script>
+        <body className={`${inter.className} bg-section`}>{children}</body>
         </html>
     );
 }
