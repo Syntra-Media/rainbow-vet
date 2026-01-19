6bd47fb8961e744fc75eea8da642c1804b092d21
Furkan <96385211+furkanesenn@users.noreply.github.com>
2026-01-19 17:52:37 +0300

    Update Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
                             Rainbow Veteriner Kliniği
                         </h2>
                         <p className={"text-sm text-black/50"}>
-                            Evcil dostlarınızın sağlığı için 7/24 yanınızdayız. Profesyonel ekibimiz ve modern ekipmanlarımızla, güvenli ve sevgi dolu bir ortamda en iyi veteriner hizmetini sunuyoruz. Bize her zaman ulaşabilirsiniz!
+                            Evcil dostlarınızın sağlığı için sizin yanınızdayız. Profesyonel ekibimiz ve modern ekipmanlarımızla, güvenli ve sevgi dolu bir ortamda en iyi veteriner hizmetini sunuyoruz. Bize her zaman ulaşabilirsiniz!
                         </p>
                         <div className={"flex gap-4 mt-2"}>
                             <a className={"flex items-center justify-center bg-rating rounded-full w-12 h-12"} href={"https://www.instagram.com/rainbow_veteriner/"}>
